3128337ef1684a975f7b54b7af3909ceba44bdac
MyheroGunji <gmpoo93.982@gmail.com>
2025-03-27 13:54:39 +0000

    first commit

---
+++ src/main/java/distsys/smarthomeproject/SmartHomeProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.smarthomeproject;
+
+/**
+ *
+ * @author 郡司真尋
+ */
+public class SmartHomeProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
