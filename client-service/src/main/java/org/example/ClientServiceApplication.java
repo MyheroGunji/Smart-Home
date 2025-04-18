8a93b95161a8ed5063ab63f3a6ce1dcee6676ba4
MyheroGunji <gmpoo93.982@gmail.com>
2025-04-18 17:08:55 +0100

    eureka server update

--- client-service/src/main/java/org/example/ClientServiceApplication.java
+++ client-service/src/main/java/org/example/ClientServiceApplication.java
@@ -2,11 +2,8 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootApplication
-@EnableEurekaServer
 public class ClientServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceApplication.class,args);
